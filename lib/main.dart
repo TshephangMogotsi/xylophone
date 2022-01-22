23b6231c23c6c7aca13e426c1e10bbfc85b35f3e
TshephangMogotsi <zinjie7@gmail.com>
2022-01-22 15:09:35 +0200

    updated UI to xylophone spec

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,105 @@ import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(const XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
   const XylophoneApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    
+  State<XylophoneApp> createState() => _XylophoneAppState();
+}
 
+class _XylophoneAppState extends State<XylophoneApp> {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: TextButton(
-           
-            onPressed: () {
-              final player = AudioCache();
-              player.play('note1.wav');
-            },
-            child: const Text("Click me"),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                  ),
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note1.wav');
+                  }, child: null,
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange,
+                  ),
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note2.wav');
+                  },
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.yellow,
+                  ),
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note3.wav');
+                  },
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.green),
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note4.wav');
+                  },
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.teal,
+                  ),
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note5.wav');
+                  },
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                  ),
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note6.wav');
+                  },
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.purple,
+                  ),
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note7.wav');
+                  },
+                  child: null,
+                ),
+              ),
+            ],
           ),
         ),
       ),
