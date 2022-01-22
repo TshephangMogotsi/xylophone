52278c9f03a8325d212523a13cc17ca11720b1a3
TshephangMogotsi <zinjie7@gmail.com>
2022-01-22 14:42:13 +0200

    created audio playing button and updated build.grade to the latest kotlin version

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(const XylophoneApp());
 
@@ -7,11 +8,20 @@ class XylophoneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: TextButton(
+           
+            onPressed: () {
+              final player = AudioCache();
+              player.play('note1.wav');
+            },
+            child: const Text("Click me"),
+          ),
         ),
       ),
     );
